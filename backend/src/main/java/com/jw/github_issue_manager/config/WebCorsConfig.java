6c2f5e78d1b7f43151819c60ce21e0265b15266a
Jungwoook <okjungwoo@gmail.com>
2026-04-09 23:23:34 +0900

    Allow all CORS requests for testing

--- backend/src/main/java/com/jw/github_issue_manager/config/WebCorsConfig.java
+++ backend/src/main/java/com/jw/github_issue_manager/config/WebCorsConfig.java
@@ -1,6 +1,5 @@
 package com.jw.github_issue_manager.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,18 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebCorsConfig implements WebMvcConfigurer {
 
-    private String[] allowedOrigins = new String[] { "http://localhost:5173" };
-
-    public void setAllowedOrigins(String[] allowedOrigins) {
-        this.allowedOrigins = allowedOrigins;
-    }
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOriginPatterns(allowedOrigins)
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-            .allowCredentials(true)
-            .allowedHeaders("*");
+            .allowedOriginPatterns("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(false);
     }
 }
